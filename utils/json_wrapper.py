ca39dfc7ee0e71f01c79ad36a6e5578898aa5508
Aishwarya <aishwarya.padmakumar.9.6@gmail.com>
2017-10-13 14:16:28 -0500

    Visual genome analysis

---
+++ utils/json_wrapper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Some useful json wrappers, mainly for interactive debugging
+
+__author__='aishwarya'
+
+import json
+
+def load_json(filename):
+    file_handle = open(filename)
+    obj = json.loads(file_handle.read())
+    file_handle.close()
+    return obj
+
+def save_json(obj, filename):
+    file_handle = open(filename, 'w')
+    json_str = json.dumps(obj)
+    file_handle.write(json_str)
+    file_handle.close()
