6cda7bd70e66b49e0ea515ff7951c12767ca4a55
Aishwarya Padmakumar <aish@cs.utexas.edu>
2017-10-26 14:08:14 -0500

    Condorized organization fo attribute features and labels

--- VisualGenome/create_classifier_data.py
+++ VisualGenome/create_classifier_data.py
@@ -5,14 +5,14 @@ from argparse import ArgumentParser
 import os
 import csv
 import random
-import sys
+import numpy as np
 
 __author__ = 'aishwarya'
 
 
 # An intermediate step where we write the list of labels (objects and attributes), and regions relevant for
 # training classifiers, given a threshold of label frequency
-def save_labels_and_regions(args):
+def organize_labels_and_regions(args):
     region_counts = dict()
 
     object_counts_filename = os.path.join(args.dataset_dir, 'region_objects_stats.csv')
@@ -154,18 +154,229 @@ def save_labels_and_regions(args):
     print 'Regions and labels identified ...'
 
 
+# Saves features for the regions in batch_regions. This is a separate function to allow parallelism
+def write_batch_features(args):
+    if args.verbose:
+        print 'Batch', args.batch_num, 'features : Started'
+
+    if args.in_train_set:
+        regions_filename = os.path.join(args.dataset_dir, 'classifiers/data/train_regions.txt')
+    else:
+        regions_filename = os.path.join(args.dataset_dir, 'classifiers/data/test_regions.txt')
+    with open(regions_filename) as regions_file:
+        regions = regions_file.read().split('\n')
+
+    start_idx = args.batch_num * args.batch_size
+    end_idx = min(start_idx + args.batch_size, len(regions))
+    batch_regions = regions[start_idx:end_idx]
+    completed_regions = list()
+
+    orig_features_folder = os.path.join(args.dataset_dir, 'regions_vgg_features')
+    orig_features_files = [os.path.join(orig_features_folder, f) for f in os.listdir(orig_features_folder)]
+    features = np.zeros((args.batch_size, args.feature_vector_size))
+
+    num_files_processed = 0
+    for input_filename in orig_features_files:
+        input_file = open(input_filename)
+        reader = csv.reader(input_file, delimiter=',')
+        for row in reader:
+            region_id = row[0].split('_')[1]
+            if region_id in batch_regions:
+                feature = row[1:]
+                region_index = batch_regions.index(region_id)
+                features[region_index] = feature
+                completed_regions.append(region_id)
+        input_file.close()
+        num_files_processed += 1
+        if args.verbose:
+            print 'Batch', args.batch_num, 'features :', num_files_processed, 'files processed'
+
+    if args.in_train_set:
+        output_filename = os.path.join(args.dataset_dir, 'classifiers/data/features/train/'
+                                       + str(args.batch_num) + '.csv')
+    else:
+        output_filename = os.path.join(args.dataset_dir, 'classifiers/data/features/test/'
+                                       + str(args.batch_num) + '.csv')
+    np.savetxt(output_filename, features)
+    print 'Batch', args.batch_num, 'features complete'
+
+
+# Saves multilabel vectors for the a batch of regions starting at start_idx.
+# This is a separate function to allow parallelism
+def write_batch_multilabels(args):
+    if args.verbose:
+        print 'Batch', args.batch_num, 'multilabel : Started'
+
+    if args.in_train_set:
+        regions_filename = os.path.join(args.dataset_dir, 'classifiers/data/train_regions.txt')
+    else:
+        regions_filename = os.path.join(args.dataset_dir, 'classifiers/data/test_regions.txt')
+    with open(regions_filename) as regions_file:
+        regions = regions_file.read().split('\n')
+
+    start_idx = args.batch_num * args.batch_size
+    end_idx = min(start_idx + args.batch_size, len(regions))
+    batch_regions = regions[start_idx:end_idx]
+
+    with open(os.path.join(args.dataset_dir, 'classifiers/data/label_names.txt')) as label_names_file:
+        label_names = label_names_file.read().split('\n')
+
+    if args.verbose:
+        print 'Batch', args.batch_num, 'multilabel : Indexing regions'
+    region_content_filename = os.path.join(args.dataset_dir, 'region_objects_unique.csv')
+    input_file = open(region_content_filename)
+    regions_with_content = dict()
+    reader = csv.reader(input_file, delimiter=',')
+    for row in reader:
+        region_id = row[0]
+        regions_with_content[region_id] = row[1:]
+
+    region_content_filename = os.path.join(args.dataset_dir, 'region_attributes_unique.csv')
+    input_file = open(region_content_filename)
+    reader = csv.reader(input_file, delimiter=',')
+    for row in reader:
+        region_id = row[0]
+        regions_with_content[region_id] = row[1:]
+    if args.verbose:
+        print 'Batch', args.batch_num, 'multilabel : Indexed regions'
+
+    if args.in_train_set:
+        output_filename = os.path.join(args.dataset_dir, 'classifiers/data/multilabels/train/'
+                                       + str(args.batch_num) + '.csv')
+    else:
+        output_filename = os.path.join(args.dataset_dir, 'classifiers/data/multilabels/test/'
+                                       + str(args.batch_num) + '.csv')
+    output_file = open(output_filename, 'w')
+    writer = csv.DictWriter(output_file, fieldnames=label_names)
+
+    num_regions_processed = 0
+    for region in batch_regions:
+        row = dict()
+        for label_name in label_names:
+            if label_name in regions_with_content[region]:
+                row[label_name] = True
+            else:
+                row[label_name] = False
+        writer.writerow(row)
+        num_regions_processed += 1
+        if args.verbose and num_regions_processed % 1000 == 0:
+            print 'Batch', args.batch_num, 'multilabel :', num_regions_processed, 'regions processed'
+
+    output_file.close()
+    print 'Batch', args.batch_num, 'multilabel complete'
+
+
+# Saves binary vector for the regions in batch_regions for the specified label.
+# This is a separate function to allow parallelism
+def write_batch_individual_label(args):
+    if args.verbose:
+        print 'Batch', args.batch_num, ', label', args.label, ' : Started'
+
+    if args.in_train_set:
+        regions_filename = os.path.join(args.dataset_dir, 'classifiers/data/train_regions.txt')
+    else:
+        regions_filename = os.path.join(args.dataset_dir, 'classifiers/data/test_regions.txt')
+    with open(regions_filename) as regions_file:
+        regions = regions_file.read().split('\n')
+
+    start_idx = args.batch_num * args.batch_size
+    end_idx = min(start_idx + args.batch_size, len(regions))
+    batch_regions = regions[start_idx:end_idx]
+
+    region_content_filename = os.path.join(args.dataset_dir, 'region_objects_unique.csv')
+    input_file = open(region_content_filename)
+    regions_with_content = dict()
+    reader = csv.reader(input_file, delimiter=',')
+    for row in reader:
+        region_id = row[0]
+        regions_with_content[region_id] = row[1:]
+
+    region_content_filename = os.path.join(args.dataset_dir, 'region_attributes_unique.csv')
+    input_file = open(region_content_filename)
+    reader = csv.reader(input_file, delimiter=',')
+    for row in reader:
+        region_id = row[0]
+        regions_with_content[region_id] = row[1:]
+
+    if args.in_train_set:
+        output_dir = os.path.join(args.dataset_dir, 'classifiers/data/binary_labels/train/' + args.label)
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        output_filename = os.path.join(output_dir, str(args.batch_num) + '.csv')
+    else:
+        output_dir = os.path.join(args.dataset_dir, 'classifiers/data/binary_labels/test/' + args.label)
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        output_filename = os.path.join(output_dir, str(args.batch_num) + '.csv')
+    output_file = open(output_filename, 'w')
+    label_values = list()
+    for region in batch_regions:
+        if args.label in regions_with_content[region]:
+            label_values.append(True)
+        else:
+            label_values.append(False)
+    writer = csv.writer(output_file)
+    writer.writerow(label_values)
+    output_file.close()
+    print 'Batch', args.batch_num, ', label =', args.label, 'complete'
+
+
 if __name__ == '__main__':
     arg_parser = ArgumentParser()
     arg_parser.add_argument('--dataset-dir', type=str, required=True,
                             help='Path to dataset')
+    arg_parser.add_argument('--verbose', action="store_true", default=False,
+                            help='Print debug output')
+
+    # Args to indicate that you need to organize labels and regions, and what is needed for this
+    arg_parser.add_argument('--organize-labels-and-regions', action="store_true", default=False,
+                            help='Some preprocessing to make actual writing of data faster. ' +
+                                 'Also creates a train-test split')
     arg_parser.add_argument('--frequency-threshold', type=int, default=1000,
                             help='Consider objects and attributes with frequency greater than this')
     arg_parser.add_argument('--train-fraction', type=float, default=0.8,
                             help='Fraction of data used for training')
     arg_parser.add_argument('--test-frequency-threshold', type=int, default=50,
                             help='Number of regions per label needed in test set')
+
+    # Select things to write
+    arg_parser.add_argument('--write-multilabels', action="store_true", default=False,
+                            help='Write multilabel vectors for a batch')
+    arg_parser.add_argument('--write-individual-labels', action="store_true", default=False,
+                            help='Write label vector for a batch for a label')
+    arg_parser.add_argument('--write-features', action="store_true", default=False,
+                            help='Write features for a batch')
+
+    # Some options common to writing multilabels, individual labels, or features
     arg_parser.add_argument('--batch-size', type=int, default=65536,
                             help='Number of data points per file (features or labels)')
+    arg_parser.add_argument('--batch-num', type=int, default=None,
+                            help='Start batch at this index in regions file')
+    arg_parser.add_argument('--in-train-set', type=bool, default=None,
+                            help='Is this part of train or test set')
+
+    arg_parser.add_argument('--label', type=str, default=None,
+                            help='Label for --write-individual-label')
+
+    arg_parser.add_argument('--feature-vector-size', type=int, default=4096,
+                            help='Dimensionality of feature vectors')
 
     args = arg_parser.parse_args()
-    save_labels_and_regions(args)
+
+    if args.organize_labels_and_regions:
+        organize_labels_and_regions(args)
+
+    if args.write_multilabels:
+        if args.batch_num is None or args.in_train_set is None:
+            raise RuntimeError('--batch_num and --in-train-set required with --write-multilabels')
+        write_batch_multilabels(args)
+
+    if args.write_individual_labels:
+        if args.batch_num is None or args.in_train_set is None or args.label is None:
+            raise RuntimeError('--batch_num, --label and --in-train-set required with --write-individual-labels')
+        write_batch_individual_label(args)
+
+    if args.write_features:
+        if args.batch_num is None or args.in_train_set is None:
+            raise RuntimeError('--batch_num and --in-train-set required with --write-features')
+        write_batch_features(args)
--- VisualGenome/create_condor_scripts.py
+++ VisualGenome/create_condor_scripts.py
@@ -2,6 +2,7 @@
 
 from argparse import ArgumentParser
 import os
+import math
 
 __author__ = 'aishwarya'
 
@@ -57,16 +58,257 @@ def extract_regions_vgg_features(args):
     condor_submit_file.close()
 
 
+# Scripts to write label vectors of multilabel data
+def write_features(args):
+    # Pairs of region file name and is_train_set
+    metadata = [
+        (os.path.join(args.dataset_dir, 'classifiers/data/train_regions.txt'), True),
+        (os.path.join(args.dataset_dir, 'classifiers/data/train_regions.txt'), False)
+    ]
+
+    condor_submit_file_name = os.path.join(*[args.dataset_dir, 'condor_scripts', args.condor_dir, 'submit.sh'])
+    condor_submit_file = open(condor_submit_file_name, 'w')
+
+    for (regions_filename, is_train_set) in metadata:
+        with open(regions_filename) as regions_file:
+            regions = regions_file.read().split('\n')
+        max_batch_num = int(math.ceil(float(len(regions)) / args.batch_size))
+
+        for batch_num in range(max_batch_num):
+            if is_train_set:
+                condor_script_file_name = os.path.join(*[args.dataset_dir, 'condor_scripts', args.condor_dir, 'train',
+                                                       str(batch_num) + '.sh'])
+            else:
+                condor_script_file_name = os.path.join(*[args.dataset_dir, 'condor_scripts', args.condor_dir, 'test',
+                                                         str(batch_num) + '.sh'])
+
+            condor_script_file = open(condor_script_file_name, 'w')
+            condor_script_file.write('universe = vanilla\n')
+            condor_script_file.write('Initialdir = ' +
+                                     '/u/aish/Documents/Research/Code/dataset_preprocessing/VisualGenome/\n')
+
+            condor_script_file.write('Executable = /lusr/bin/python\n')
+
+            condor_script_file.write('Arguments = create_classifier_data.py \\\n')
+            condor_script_file.write('\t\t --dataset-dir=/scratch/cluster/aish/VisualGenome \\\n')
+            condor_script_file.write('\t\t --write-features \\\n')
+            condor_script_file.write('\t\t --batch-num=' + str(batch_num) + ' \\\n')
+            condor_script_file.write('\t\t --in-train-set=' + str(is_train_set) + ' \\\n')
+            condor_script_file.write('\t\t --verbose\n')
+
+            condor_script_file.write('+Group   = "GRAD"\n')
+            condor_script_file.write('+Project = "AI_ROBOTICS"\n')
+            condor_script_file.write('+ProjectDescription = "VisualGenome - Writing features"\n')
+            condor_script_file.write('JobBatchName = "VisualGenome - Writing features"\n')
+            condor_script_file.write('Requirements = InMastodon\n')
+
+            if is_train_set:
+                condor_script_file.write('Log = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                   'train/log', str(batch_num) + '.log']) + '\n')
+                condor_script_file.write('Error = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                     'train/err', str(batch_num) + '.err']) + '\n')
+                condor_script_file.write('Output = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                      'train/out', str(batch_num) + '.out']) + '\n')
+            else:
+                condor_script_file.write('Log = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                   'test/log', str(batch_num) + '.log']) + '\n')
+                condor_script_file.write('Error = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                     'test/err', str(batch_num) + '.err']) + '\n')
+                condor_script_file.write('Output = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                      'test/out', str(batch_num) + '.out']) + '\n')
+
+            condor_script_file.write('Notification = complete\n')
+            condor_script_file.write('Notify_user = aish@cs.utexas.edu\n')
+            condor_script_file.write('Queue 1\n')
+            condor_script_file.close()
+
+            condor_submit_file.write('condor_submit ' + condor_script_file_name + '\n')
+
+    condor_submit_file.close()
+
+
+# Scripts to write label vectors of multilabel data
+def write_multilabels(args):
+    # Pairs of region file name and is_train_set
+    metadata = [
+        (os.path.join(args.dataset_dir, 'classifiers/data/train_regions.txt'), True),
+        (os.path.join(args.dataset_dir, 'classifiers/data/train_regions.txt'), False)
+    ]
+
+    condor_submit_file_name = os.path.join(*[args.dataset_dir, 'condor_scripts', args.condor_dir, 'submit.sh'])
+    condor_submit_file = open(condor_submit_file_name, 'w')
+
+    for (regions_filename, is_train_set) in metadata:
+        with open(regions_filename) as regions_file:
+            regions = regions_file.read().split('\n')
+        max_batch_num = int(math.ceil(float(len(regions)) / args.batch_size))
+
+        for batch_num in range(max_batch_num):
+            if is_train_set:
+                condor_script_file_name = os.path.join(*[args.dataset_dir, 'condor_scripts', args.condor_dir, 'train',
+                                                       str(batch_num) + '.sh'])
+            else:
+                condor_script_file_name = os.path.join(*[args.dataset_dir, 'condor_scripts', args.condor_dir, 'test',
+                                                         str(batch_num) + '.sh'])
+
+            condor_script_file = open(condor_script_file_name, 'w')
+            condor_script_file.write('universe = vanilla\n')
+            condor_script_file.write('Initialdir = ' +
+                                     '/u/aish/Documents/Research/Code/dataset_preprocessing/VisualGenome/\n')
+
+            condor_script_file.write('Executable = /lusr/bin/python\n')
+
+            condor_script_file.write('Arguments = create_classifier_data.py \\\n')
+            condor_script_file.write('\t\t --dataset-dir=/scratch/cluster/aish/VisualGenome \\\n')
+            condor_script_file.write('\t\t --write-multilabels \\\n')
+            condor_script_file.write('\t\t --batch-num=' + str(batch_num) + ' \\\n')
+            condor_script_file.write('\t\t --in-train-set=' + str(is_train_set) + ' \\\n')
+            condor_script_file.write('\t\t --verbose\n')
+
+            condor_script_file.write('+Group   = "GRAD"\n')
+            condor_script_file.write('+Project = "AI_ROBOTICS"\n')
+            condor_script_file.write('+ProjectDescription = "VisualGenome - Writing multilabel data"\n')
+            condor_script_file.write('JobBatchName = "VisualGenome - Writing multilabel data"\n')
+            condor_script_file.write('Requirements = InMastodon\n')
+
+            if is_train_set:
+                condor_script_file.write('Log = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                   'train/log', str(batch_num) + '.log']) + '\n')
+                condor_script_file.write('Error = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                     'train/err', str(batch_num) + '.err']) + '\n')
+                condor_script_file.write('Output = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                      'train/out', str(batch_num) + '.out']) + '\n')
+            else:
+                condor_script_file.write('Log = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                   'test/log', str(batch_num) + '.log']) + '\n')
+                condor_script_file.write('Error = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                     'test/err', str(batch_num) + '.err']) + '\n')
+                condor_script_file.write('Output = ' + os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                                      'test/out', str(batch_num) + '.out']) + '\n')
+
+            condor_script_file.write('Notification = complete\n')
+            condor_script_file.write('Notify_user = aish@cs.utexas.edu\n')
+            condor_script_file.write('Queue 1\n')
+            condor_script_file.close()
+
+            condor_submit_file.write('condor_submit ' + condor_script_file_name + '\n')
+
+    condor_submit_file.close()
+
+
+# Scripts to write label vectors of multilabel data
+def write_individual_labels(args):
+    # Pairs of region file name and is_train_set
+    metadata = [
+        (os.path.join(args.dataset_dir, 'classifiers/data/train_regions.txt'), True),
+        (os.path.join(args.dataset_dir, 'classifiers/data/train_regions.txt'), False)
+    ]
+
+    with open(os.path.join(args.dataset_dir, 'classifiers/data/label_names.txt')) as label_names_file:
+        label_names = label_names_file.read().split('\n')
+
+    condor_submit_file_name = os.path.join(*[args.dataset_dir, 'condor_scripts', args.condor_dir, 'submit.sh'])
+    condor_submit_file = open(condor_submit_file_name, 'w')
+
+    num_iterations_finished = 0
+    for (regions_filename, is_train_set) in metadata:
+        for label in label_names:
+            with open(regions_filename) as regions_file:
+                regions = regions_file.read().split('\n')
+            max_batch_num = int(math.ceil(float(len(regions)) / args.batch_size))
+
+            for batch_num in range(max_batch_num):
+                if is_train_set:
+                    condor_script_file_name = os.path.join(*[args.dataset_dir, 'condor_scripts', args.condor_dir, 'train',
+                                                           str(batch_num) + '_' + label + '.sh'])
+                else:
+                    condor_script_file_name = os.path.join(*[args.dataset_dir, 'condor_scripts', args.condor_dir, 'test',
+                                                             str(batch_num) + '_' + label + '.sh'])
+
+                condor_script_file = open(condor_script_file_name, 'w')
+                condor_script_file.write('universe = vanilla\n')
+                condor_script_file.write('Initialdir = ' +
+                                         '/u/aish/Documents/Research/Code/dataset_preprocessing/VisualGenome/\n')
+
+                condor_script_file.write('Executable = /lusr/bin/python\n')
+
+                condor_script_file.write('Arguments = create_classifier_data.py \\\n')
+                condor_script_file.write('\t\t --dataset-dir=/scratch/cluster/aish/VisualGenome \\\n')
+                condor_script_file.write('\t\t --write-individual-labels \\\n')
+                condor_script_file.write('\t\t --batch-num=' + str(batch_num) + ' \\\n')
+                condor_script_file.write('\t\t --label=' + label + ' \\\n')
+                condor_script_file.write('\t\t --in-train-set=' + str(is_train_set) + ' \\\n')
+                condor_script_file.write('\t\t --verbose\n')
+
+                condor_script_file.write('+Group   = "GRAD"\n')
+                condor_script_file.write('+Project = "AI_ROBOTICS"\n')
+                condor_script_file.write('+ProjectDescription = "VisualGenome - Writing individual label data"\n')
+                condor_script_file.write('JobBatchName = "VisualGenome - Writing individual label data"\n')
+                condor_script_file.write('Requirements = InMastodon\n')
+
+                if is_train_set:
+                    condor_script_file.write('Log = ' +
+                                             os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                          'train/log', str(batch_num) + '_' + label + '.log']) + '\n')
+                    condor_script_file.write('Error = ' +
+                                             os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                          'train/err', str(batch_num) + '_' + label + '.err']) + '\n')
+                    condor_script_file.write('Output = ' +
+                                             os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                          'train/out', str(batch_num) + '_' + label + '.out']) + '\n')
+                else:
+                    condor_script_file.write('Log = ' +
+                                             os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                          'test/log', str(batch_num) + '_' + label + '.log']) + '\n')
+                    condor_script_file.write('Error = ' +
+                                             os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                          'test/err', str(batch_num) + '_' + label + '.err']) + '\n')
+                    condor_script_file.write('Output = ' +
+                                             os.path.join(*[args.dataset_dir, 'condor_log', args.condor_dir,
+                                                          'test/out', str(batch_num) + '_' + label + '.out']) + '\n')
+
+                condor_script_file.write('Notification = complete\n')
+                condor_script_file.write('Notify_user = aish@cs.utexas.edu\n')
+                condor_script_file.write('Queue 1\n')
+                condor_script_file.close()
+
+                condor_submit_file.write('condor_submit ' + condor_script_file_name + '\n')
+
+                num_iterations_finished += 1
+                if num_iterations_finished % 1000 == 0:
+                    print num_iterations_finished
+
+    condor_submit_file.close()
+
+
 if __name__ == '__main__':
     arg_parser = ArgumentParser()
     arg_parser.add_argument('--dataset-dir', type=str, required=True,
                             help='Visual Genome dataset dir')
     arg_parser.add_argument('--condor-dir', type=str, required=True,
                             help='Subdirectory under dataset-dir/condor_scripts and dataset-dir/condor_log')
+
     arg_parser.add_argument('--extract-regions-vgg-features', action="store_true", default=False,
                             help='Extract VGG features of regions')
+
+    arg_parser.add_argument('--batch-size', type=int, default=65536,
+                            help='Number of data points per file (features or labels)')
+    arg_parser.add_argument('--write-multilabels', action="store_true", default=False,
+                            help='Write multilabel vectors for a batch')
+    arg_parser.add_argument('--write-individual-labels', action="store_true", default=False,
+                            help='Write label vectors for a batch for a label')
+    arg_parser.add_argument('--write-features', action="store_true", default=False,
+                            help='Write features for a batch')
 
     args = arg_parser.parse_args()
     if args.extract_regions_vgg_features:
         extract_regions_vgg_features(args)
 
+    if args.write_multilabels:
+        write_multilabels(args)
+
+    if args.write_individual_labels:
+        write_individual_labels(args)
+
+    if args.write_features:
+        write_features(args)
