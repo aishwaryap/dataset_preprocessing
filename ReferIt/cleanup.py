117e0dd6a81f7d52b181e3e58dd1ec696a2ea847
Aishwarya <aishwarya.padmakumar.9.6@gmail.com>
2019-03-21 11:12:42 -0500

    Move remote edgebox files

--- ReferIt/cleanup.py
+++ ReferIt/cleanup.py
@@ -9,6 +9,8 @@ import re
 import h5py
 import traceback
 import shutil
+import paramiko
+from scp import SCPClient
 
 
 def move():
@@ -32,6 +34,38 @@ def move_edgeboxes(image_list_file, orig_dir, target_dir):
             orig_file = os.path.join(orig_dir, filename)
             new_file = os.path.join(target_dir, filename)
             shutil.move(orig_file, new_file)
+
+
+def move_edgeboxes_remote(image_list_file, orig_dir, remote_target_dir, ssh_host, ssh_user):
+    with open(image_list_file) as handle:
+        image_list = set(handle.read().splitlines())
+    ssh = None
+    client = None
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.load_system_host_keys()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=ssh_host, username=ssh_user)
+        client = SCPClient(ssh.get_transport())
+        for filename in os.listdir(orig_dir):
+            print('Processing:', filename)
+            if re.sub('.hdf5', '', filename) in image_list:
+                print('\tMoving', filename)
+                local_path = os.path.join(orig_dir, filename)
+                remote_path = os.path.join(remote_target_dir, filename)
+                client.put(local_path, remote_path)
+                os.remove(local_path)
+    except Exception:
+        if ssh:
+            ssh.close()
+        if client:
+            client.close()
+        raise
+    finally:
+        if ssh:
+            ssh.close()
+        if client:
+            client.close()
 
 
 def rename():
@@ -88,23 +122,19 @@ def fix_edgebox_hdf5():
         checked_files_handle.write(filename + '\n')
         checked_files_handle.flush()
 
-
     bad_files_handle.close()
     checked_files_handle.close()
 
 
 if __name__ == '__main__':
-    # fix_edgebox_hdf5()
+    orig_dir = '/scratch/cluster/aish/ReferIt/resnet_fcn_features/edgeboxes/'
 
-    # image_list_file = '/u/aish/Documents/ReferIt_link_T5/split/referit_test_imlist.txt'
-    # orig_dir = '/u/aish/Documents/ReferIt_link/resnet_fcn_features/edgeboxes/'
-    # target_dir = '/u/aish/Documents/ReferIt_link_T5/resnet_fcn_features/edgeboxes/'
-    # move_edgeboxes(image_list_file, orig_dir, target_dir)
-    image_list_file = '/u/aish/Documents/ReferIt_link_T5/split/referit_train_imlist.txt'
-    orig_dir = '/u/aish/Documents/ReferIt_link_T5/resnet_fcn_features/edgeboxes/'
+    image_list_file = '/scratch/cluster/aish/ReferIt/split/referit_test_imlist.txt'
+    target_dir = '/u/aish/Documents/ReferIt_link_T5/resnet_fcn_features/edgeboxes/'
+    move_edgeboxes_remote(image_list_file, orig_dir, target_dir, 'hati', 'aish')
+    image_list_file = '/scratch/cluster/aish/ReferIt/split/referit_train_imlist.txt'
     target_dir = '/u/aish/Documents/ReferIt_link/resnet_fcn_features/edgeboxes/'
-    move_edgeboxes(image_list_file, orig_dir, target_dir)
-    # image_list_file = '/u/aish/Documents/ReferIt_link_T5/split/referit_val_imlist.txt'
-    # orig_dir = '/u/aish/Documents/ReferIt_link/resnet_fcn_features/edgeboxes/'
-    # target_dir = '/u/aish/Documents/ReferIt_link_T5/resnet_fcn_features/edgeboxes/'
-    # move_edgeboxes(image_list_file, orig_dir, target_dir)
+    move_edgeboxes_remote(image_list_file, orig_dir, target_dir, 'hati', 'aish')
+    image_list_file = '/scratch/cluster/aish/ReferIt/split/referit_val_imlist.txt'
+    target_dir = '/u/aish/Documents/ReferIt_link_T5/resnet_fcn_features/edgeboxes/'
+    move_edgeboxes_remote(image_list_file, orig_dir, target_dir, 'hati', 'aish')
